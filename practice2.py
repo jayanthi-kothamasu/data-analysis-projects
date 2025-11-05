2d7b176ceb22055e35444ec3e0b56ec95cddaea3
Jayanthi <jayanthi.kothamasu@gmail.com>
2025-11-04 20:41:36 -0600

    Completed

---
+++ practice2.py
@@ -0,0 +1,29 @@
+'''
+score = 88
+attendence = 0.92
+passed = (score>= 70) and (attendence >=0.90)
+print("passsed?" ,passed)
+'''
+'''
+for i in range (5):
+    print ("index",i)
+'''
+
+
+''''''
+for char in "data": 
+    print(char)
+ 
+    ''''''
+
+word = "python"
+print(word[5])
+
+color= "green"
+for i in range(len(color)):
+   print(f"index{i} contains 'color{i}'")
+
+for i in range(3):
+  print("processing" ,{i})
+
+     
